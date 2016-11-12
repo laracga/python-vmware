9209191e6016a1db63ceb917c25ad1b594c2f657
Lara Cancela <laracg.mar@gmail.com>
2016-11-12 10:29:28 +0100

    vmware sin maquinas de vg

--- scripts-py/test-snapshots.sh
+++ scripts-py/test-snapshots.sh
@@ -9,20 +9,16 @@ rm -f /tmp/nodes-snapshot.txt
 echo "************************** Node Snapshot *****************************"
 
 cd /var/lib/jenkins
-#knife search node "chef_environment:solium" | grep "Node Name" | awk -F' ' '{ print $3 }' >> /tmp/nodes.txt
-knife search node "name:vgotrs02 OR name:vgnagioscnt01" | grep "Node Name" | awk -F' ' '{ print $3 }' >> /tmp/nodes.txt
+knife search node "name:vgtest01 OR name:vgtest02" | grep "Node Name" | awk -F' ' '{ print $3 }' >> /tmp/nodes.txt
 
-#fecha=`date +%Y-%m-%d`
 fecha=`date +%Y-%m`
 
 while IFS= read -r line
 do
   echo "$line" 
   cd /var/Tools
-  #python snapshot_tool.py -s $vcenter -u lcancela@soliumes -p susiyz07. -m $line create -sn $fecha-$line -sd $fecha-$line
 
   echo "check snapshot"
-  cd /var/Tools
   echo $fecha-$line
 
   #Esperamos a que se haga el snapshot
